723f09db489ad1e560b0cde7bde684d7003370b0
Remi Schwartz <schwartz.remi@gmail.com>
2020-11-11 15:38:17 +0100

    app operationnal, with new review on show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "belgian" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+chez_nico =  { name: "Chez Nico", address: "en face", category: "italian" }
+food_cheri =  { name: "Food Chéri", address: "chez toi", category: "french" }
+traviatta =  { name: "Traviatta", address: "PAM", category: "italian" }
+
+[ dishoom, pizza_east, chez_nico, food_cheri, traviatta ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
